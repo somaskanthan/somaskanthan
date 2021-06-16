98652d3f89e8a8f146e70a62bee4fe42eab9ed0a
somas__kanthan <85994913+somaskanthan@users.noreply.github.com>
2021-06-16 17:23:43 +0530

    BIRH DAY WISHE TO MY FRIEND AND FAMILY

---
+++ birth day wishes.cpp
@@ -0,0 +1,24 @@
+#include <iomanip>
+#include<iostream>
+#include <stdlib.h>
+#include <windows.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+   	string data;
+	cout<<"\t\t\t\t\t\tWELCOME MACHI\n"<<"\n\n\n machi type your name here ; then press enter key\n";
+	getline(cin,data);
+	     system("color F5"); 
+	cout<<"\n";
+	system("cls");
+	cout<<"\n\n\n\n"<<"\n\t\t\t\t\t   WELCOME "<< data<<"\n\n";;
+	Sleep(7000);
+	
+	cout<<"\t\t\t@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@\n"<<"\t\t\t#\t\t\t\t\t\t\t#\n"<<"\t\t\t#\t\t\t\t\t\t\t#\n"<<"\t\t\t#\t\t\t\t\t\t\t#\n"<<"\t\t\t#"<<"\t\t HAPPY BIRTHDAY MACHI\t\t\t#\n"<<"\t\t\t#\t\t\t\t\t\t\t#\n"
+        <<"\t\t\t#\t\t\t\t\t\t\t#\n"<<"\t\t\t#"<<"\t\t LIFE LONG STAY HAPPY\t\t\t#\n"<<"\t\t\t#\t\t\t\t\t\t\t#\n"<<"\t\t\t#\t\t\t\t\t\t\t#\n"<<"\t\t\t#"<<"\t\t     GOD BLESS YOU\t\t\t#\n"<<"\t\t\t#\t\t\t\t\t\t\t#\n"<<"\t\t\t#\t\t\t\t\t\t\t#\n"
+		<<"\t\t\t@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@_@\n\n\n\n\n\n\n\n\n\n\n";
+		
+		 
+}
