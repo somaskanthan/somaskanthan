97c5eee574aaa58fe798704cdc8a1722899a4b46
somas__kanthan <85994913+somaskanthan@users.noreply.github.com>
2021-06-28 11:47:24 +0530

    Update attendence.cpp

--- attendence.cpp
+++ attendence.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
+#include<stdio.h>
 #include <string>
 using namespace std;
-
 
 int main ()
 {
 	
-    string student[3];
+	string student[3];
+	string result[3];
     
-     for(int i=0;i<3;i++)
-    {
-     cin>>student[i];
+    for(int i=0;i<3;i++)
+	{
+   	cin>>student[i];
     }
-  	string result;
-  	string a,b,c;
-     for(int j=0;j<3;j++)
-     {
-	 cout<<student[j]<<" "<<"are you there"<<"\n";
-	 cin>>result;
-     }
+  
+    for(int j=0;j<3;j++)
+	{
+	  cout<<student[j]<<" "<<"are you there"<<"\n";
+	  cin>>result[j];
+    }
+	  
+    system("cls");
+	  
+    for(int k=0;k<3;k++)
+    {
+    	
+	cout<<student[k]<<" is :";
+	cout<<result[k]<<"\n\n";
+	
+    }
 
 }
     
-
-	
-	
-
-
-
